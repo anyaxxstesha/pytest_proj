d38059b180bf9e7f3bfc82bd1c95aaabe2d01277
Anna Tarazanova <an.tarazanova2016@yandex.ru>
2024-02-18 00:40:08 +0300

    add fixture to tests for get_value function

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -1,5 +1,6 @@
 from utils import arrs
 from utils import dicts
+import pytest
 
 
 def test_get():
@@ -14,8 +15,13 @@ def test_slice():
     assert arrs.my_slice([1, 2, 3, 4], None, 3) == [1, 2, 3]
 
 
-def test_get_val():
-    assert dicts.get_val({1: 'one', 2: 'two', 3: 'three'}, 2, 'git') == 'two'
-    assert dicts.get_val({1: 'one', 2: 'two', 3: 'three'}, 4, 'git') == 'git'
-    assert dicts.get_val({1: 'one', 2: 'two', 3: 'three'}, 4, 'another') == 'another'
-    assert dicts.get_val({1: 'one', 2: 'two', 3: 'three'}, 2, None) == 'two'
+@pytest.fixture
+def dic():
+    return {1: 'one', 2: 'two', 3: 'three'}
+
+
+def test_get_val(dic):
+    assert dicts.get_val(dic, 2, 'git') == 'two'
+    assert dicts.get_val(dic, 4, 'git') == 'git'
+    assert dicts.get_val(dic, 4, 'another') == 'another'
+    assert dicts.get_val(dic, 2, None) == 'two'
